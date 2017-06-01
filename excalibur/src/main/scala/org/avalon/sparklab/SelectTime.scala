ca2299f0104625eac246c5e1da71058657225770
Avalon <Avalon@Avalon-PC>
2017-06-01 20:47:56 +0800

    fix

--- excalibur/src/main/scala/org/avalon/sparklab/SelectTime.scala
+++ excalibur/src/main/scala/org/avalon/sparklab/SelectTime.scala
@@ -9,9 +9,17 @@ import org.apache.spark.sql.SparkSession
 import com.sun.org.apache.xalan.internal.xsltc.compiler.ForEach
 import org.apache.spark.sql.DataFrame
 
+import java.util.Date
+
 object SelectTime {
+
   
+  case class Person(name: String, age: Long)
   
+  //Date
+  case class IFMinData(time: String, openPrice: Double, highestPrice: Double, lowestPrice: Double, closePrice: Double,
+                       tradingAmount: Double, volume: Long)
+
   def main(args: Array[String]) /*: Unit = */ {
 
     // warehouseLocation points to the default location for managed databases and tables
@@ -21,22 +29,62 @@ object SelectTime {
     val spark = SparkSession
       .builder()
       .appName("Spark Example")
-      .master("spark://local")
+      .master("local")
       .getOrCreate()
 
     import spark.implicits._
     import spark.sql
-
-    val df = spark.read.format("csv").option("header", true).load(System.getProperty("user.dir")+"/data/47#IFL8.lc1.csv")
+    
+    /*val path = System.getProperty("user.dir")+"/src/main/resources/people.json"
+    val peopleDS = spark.read.json(path).as[Person]
+    peopleDS.show()*/
 
     
+    //读取csv转换为json
+    val df = spark.read.format("csv")
+     
+     .option("quote", "\"")
+     .option("escape", "")
+     .option("timestampFormat", "yyyy-MM-dd HH:mm:ss")
+     .option("header", true)
+     .load(System.getProperty("user.dir") + "/data/1.csv")
+    //.select("openPrice", "volume")
     val it = df.toLocalIterator()
+		//println(it.next())
+		
+		df.write.format("json").save(System.getProperty("user.dir") + "/data/2.json")
+		
+		//df.write.format("json").
+	
+		
+    //执行json的解读
+		/*val dfs = spark.read.json(System.getProperty("user.dir") + "/data/1.json")
+		
+		val ds = dfs.as[IFMinData]
+    ds.show()*/
+		//println(ds.toLocalIterator().next())
     
     
-    
+    //val ds = spark.read.option("header", true).csv(System.getProperty("user.dir") + "/data/47#IFL8.lc1.csv").as[IFMinData]
+    //val ds = spark.read.format("csv").option("header", true).load(System.getProperty("user.dir") + "/data/47#IFL8.lc1.csv").as[IFMinData]
 
+    
+		//执行csv的解读
+    /*val ds = spark.read
+      .option("header", true)
+      .option("quote", '\u0000')
+      .option("timestampFormat", "yyyy-MM-dd HH:mm:ss")
+      .csv(System.getProperty("user.dir") + "/data/1.csv")
+      .as[IFMinData]
+    
+    ds.show()*/
+    
+    /*val it = ds.toLocalIterator()
+    while (it.hasNext()) {
+      println(it.next())
+    }*/
+    
 
   }
-  
-  
+
 }
